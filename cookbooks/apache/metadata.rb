f538e13e5c544ee45a6847a2af700ebecc73f9fd
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-12-12 14:09:07 +0200

    added hello_world dependency

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures apache'
 version '0.1.0'
 chef_version '>= 16.0'
 
+depends 'hello_world'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.

54f6ae99b8e2bd3181fee12ed55ddc9e9255bef2
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-12-12 13:31:59 +0200

    added first recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+
+# A package resource named "apache2", with actions install
+package 'apache2' do
+  action :install
+end
+
+# A service resource named "apache2", with actions enable and then start (order matters)
+service 'apache2' do
+  action [ :enable, :start]
+end
+
+# A cookbook_file resource
+cookbook_file '/var/www/html/index.html' do
+  source 'regularindex.html'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
